2f07f8ffacab56f8dcf8b144e0ca90e2749ffbf6
b <3>
2023-02-21 16:00:57 +0700

    20.1 20.2

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,13 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+double remx,remy;
 
+remx = min(R1->x+R1->w,R2->x+R2->w)-max(R1->x,R2->x);
+remy = min(R1->y,R2->y)-max(R1->y-R1->h,R2->y-R2->h);
 
+if(remx < 0 || remy < 0) return 0;
+else return remx*remy;
 
 }
